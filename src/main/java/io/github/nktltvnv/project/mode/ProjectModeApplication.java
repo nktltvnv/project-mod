5500339bcbb2edbc21a9ba53d44acd8e134c1dbe
Nikita Litvinov <nktltvnv.v@gmail.com>
2025-01-19 21:33:03 +0200

    initialized Spring Boot project

---
+++ src/main/java/io/github/nktltvnv/project/mode/ProjectModeApplication.java
@@ -0,0 +1,13 @@
+package io.github.nktltvnv.project.mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectModeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nktltvnv/project/mode/ProjectModeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nktltvnv.project.mode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
